4292654a8946615d5e8f4b679787522f809befbe
José Ojeda <jojeda5171@uta.edu.ec>
2023-08-10 11:46:26 -0500

    Theme

--- yes_no_app/lib/main.dart
+++ yes_no_app/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'config/theme/app_theme.dart';
+
 void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
@@ -10,13 +12,14 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Yes No App',
       debugShowCheckedModeBanner: false,
+      theme: AppTheme(selectedColor: 6).theme(),
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Material App Bar'),
+          title: const Center(child: Text('Material App Bar')),
         ),
         body: Center(
             child: FilledButton.tonal(
-                onPressed: () {}, child: const Text('Click me'))),
+                onPressed: () {}, child: const Text('Click me!'))),
       ),
     );
   }
