0df8fdc4630b698dacd69c5938648468e833fa93
José Ojeda <jojeda5171@uta.edu.ec>
2023-08-11 12:37:39 -0500

    ListView en la pantalla chat

--- yes_no_app/lib/presentation/chat/chat_screen.dart
+++ yes_no_app/lib/presentation/chat/chat_screen.dart
@@ -14,7 +14,33 @@ class ChatScreen extends StatelessWidget {
                 'https://upload.wikimedia.org/wikipedia/commons/b/b4/Lionel-Messi-Argentina-2022-FIFA-World-Cup_%28cropped%29.jpg'),
           ),
         ),
-        title: const Text('Hello World'),
+        title: const Text('Leo Messi'),
+      ),
+      body: const _ChatView(),
+    );
+  }
+}
+
+class _ChatView extends StatelessWidget {
+  const _ChatView();
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10),
+        child: Column(
+          children: [
+            Expanded(
+                child: ListView.builder(
+              itemCount: 100,
+              itemBuilder: (context, index) {
+                return Text('Indice: $index');
+              },
+            )),
+            const Text('Mundo')
+          ],
+        ),
       ),
     );
   }
