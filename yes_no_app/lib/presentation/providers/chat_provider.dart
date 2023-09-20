149ad7100b0288b44e3bf281fd04b51004dfbdac
José Ojeda <jojeda5171@uta.edu.ec>
2023-09-19 22:44:09 -0500

    scroll al final cuando se envien sms

--- yes_no_app/lib/presentation/providers/chat_provider.dart
+++ yes_no_app/lib/presentation/providers/chat_provider.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
-
 import '../../domain/entities/message.dart';
 
 class ChatProvider extends ChangeNotifier {
+  final ScrollController chatScrollController = ScrollController();
+
   List<Message> messageList = [
     Message(text: 'Hola amor!', fromWho: FromWho.mine),
     Message(text: 'Ya regresaste del trabajo?', fromWho: FromWho.mine)
@@ -13,5 +14,14 @@ class ChatProvider extends ChangeNotifier {
     messageList.add(newMessage);
 
     notifyListeners();
+    moveScrollToBottom();
+  }
+
+  Future<void> moveScrollToBottom() async {
+    await Future.delayed(const Duration(milliseconds: 100));
+    chatScrollController.animateTo(
+        chatScrollController.position.maxScrollExtent,
+        duration: const Duration(microseconds: 300),
+        curve: Curves.easeOut);
   }
 }
